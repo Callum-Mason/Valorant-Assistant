4746a78a6e1ae71045ed690cc272027bbc20b5e9
Callum Mason <callum.mason@isitetv.com>
2022-05-12 16:29:29 +0100

    moved the Change Resolution to its own commponant

---
+++ src/components/ChangeResolution.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container class="Resolution">
+    <v-select
+      v-model="resolution"
+      :items="items"
+      label="Resolution"
+      data-vv-name="resolution"
+      @change="ChangeResolution"
+    ></v-select>
+  </v-container>
+</template>
+
+<script>
+import { appWindow, PhysicalSize } from "@tauri-apps/api/window";
+
+export default {
+  name: "ChangeResolution",
+  data() {
+    return {
+      resolution: null,
+      items: ["1920x1080", "1280x720", "1024x768", "800x600"],
+    };
+  },
+  methods: {
+    ChangeResolution() {
+      console.log(
+        "Resolution - x: " +
+          this.resolution.split("x")[0] +
+          " y: " +
+          this.resolution.split("x")[1]
+      );
+      appWindow.setSize(
+        new PhysicalSize(
+          parseInt(this.resolution.split("x")[0]),
+          parseInt(this.resolution.split("x")[1])
+        )
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,8 @@
     <br>
     <v-btn @click="increment">Click me</v-btn>
     <br>
-    <v-select
-          v-model="resolution"
-          :items="items"
-          label="Resolution"
-          data-vv-name="resolution"
-          @change="ChangeResolution"
-        ></v-select>
+    <ChangeResolution/>
+
 
 
 
@@ -23,12 +18,13 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import { appWindow, PhysicalSize } from "@tauri-apps/api/window";
+import ChangeResolution from '@/components/ChangeResolution.vue'
 
 export default {
   name: "HomeView",
   components: {
     // HelloWorld
+    ChangeResolution
   },
   data() {
     return {
@@ -47,22 +43,11 @@ export default {
     increment() {
       this.count++;
       console.log(this.count);
-      // document.title = "Demo App : Count" + this.count;
-      appWindow.setTitle("Demo App : Count " + this.count);
     },
     decrement() {
       this.count--;
       console.log(this.count);
-      appWindow.setTitle("Demo App : Count " + this.count);
     },
-    ChangeResolution() {
-      console.log("ChangeResolution");
-      console.log(this.resolution.split('x')[0] + ' y: ' + this.resolution.split('x')[1]);
-      appWindow.setSize(new PhysicalSize(parseInt(this.resolution.split('x')[0]), parseInt(this.resolution.split('x')[1])));
-    },
-    close(){
-      appWindow.close();
-    }
   },
   mounted() {
     // methods can be called in lifecycle hooks, or other methods!
