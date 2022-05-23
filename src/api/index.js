276dfb1c8e49811dda5424a372ec46a7fed2db3b
Callum Luke Mason <clmason99@hotmail.co.uk>
2022-05-23 22:01:02 +0100

    cleaned up setup

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 var {
-	// GetTokens,
-	getLockFile
+	getLockFile,
+	// getTokens
 } = require('./GetTokens');
+
 
 // var lockFileName = "";
 // var lockFilepid = "";
@@ -14,46 +15,23 @@ async function setup() {
 	// console.log(getLockFile());
 
 
-	await getLockFile().then(async (lockfiledata) => {
+	await getLockFile().then(function (lockfiledata) {
 
 
-		console.log(lockfiledata);
-
+		// console.log(lockfiledata);
 
 		const User = 'riot';
 		const LockFilePassword = lockfiledata.lockFilePassword;
 		const LockFilePort = lockfiledata.lockFilePort;
-		// const LockFilepid = data.pid;
-		// const LockFileprotocol = data.protocol;
-		// const LockFileName = data.name;
+		// const LockFilepid = lockfiledata.pid;
+		// const LockFileprotocol = lockfiledata.protocol;
+		// const LockFileName = lockfiledata.name;
 		let accessToken = ""; //Gets Set
 		let PUUID = ""; //Gets Set
 		let entitlements = ""; //Gets Set
 		let Region = "eu";
 		let ClientVersion = "release-04.09-3-708238";
 		let ClientPlatform = "";
-
-
-
-		// var AccessTokenConfig = {
-		// 	method: "get",
-		// 	url: `https://127.0.0.1:${LockFilePort}/entitlements/v1/token`,
-		// 	auth: {
-		// 		username: User,
-		// 		password: LockFilePassword,
-		// 	},
-		// };
-
-		// var PUUIDConfig = {
-		// 	method: "get",
-		// 	url: `https://127.0.0.1:${LockFilePort}/chat/v1/session`,
-		// 	auth: {
-		// 		username: User,
-		// 		password: LockFilePassword,
-		// 	},
-		// };
-
-		// let configEntitlement = {};
 
 		localStorage.setItem(
 			"Valorant",
@@ -71,65 +49,11 @@ async function setup() {
 		);
 
 
-		// GetTokens(AccessTokenConfig)
-		// 	.then(function (accessTokendata) {
-		// 		let accessToken = accessTokendata.accessToken;
-		// 		console.log('Access Token: ' + accessToken);
-		// 		var Valorant = JSON.parse(localStorage.getItem('Valorant'));
-		// 		Valorant.accessToken = accessToken;
-		// 		localStorage.setItem('config', JSON.stringify(Valorant));
 
-
-		// 		configEntitlement = {
-		// 			method: "post",
-		// 			url: "https://entitlements.auth.riotgames.com/api/token/v1",
-		// 			headers: {
-		// 				"Content-Type": "application/json",
-		// 				Authorization: `Bearer ${accessToken}`,
-		// 			},
-		// 		};
-		// 	})
-		// 	.catch(function (error) {
-		// 		console.log("Error code: " + error.code);
-		// 	})
-		// 	.then(function () {
-		// 		GetTokens(PUUIDConfig)
-		// 			.then(function (puuiddata) {
-		// 				PUUID = puuiddata.puuid;
-		// 				// console.log('PUUID: ' + PUUID);
-		// 				var Valorant = JSON.parse(localStorage.getItem('Valorant'));
-		// 				Valorant.PUUID = PUUID;
-		// 				localStorage.setItem('config', JSON.stringify(Valorant));
-
-
-		// 			})
-		// 			.catch(function (error) {
-		// 				console.log("Error code: " + error.code);
-		// 			});
-		// 	})
-		// 	.catch(function (error) {
-		// 		console.log("Error code: " + error.code);
-		// 	})
-		// 	.then(function () {
-		// 		GetTokens(configEntitlement)
-		// 			.then(function (entitlementsdata) {
-		// 				entitlements = entitlementsdata.entitlements_token;
-		// 				console.log('Entitlements: ' + entitlements);
-		// 				var Valorant = JSON.parse(localStorage.getItem('Valorant'));
-		// 				Valorant.entitlements = entitlements;
-		// 				localStorage.setItem('config', JSON.stringify(Valorant));
-		// 			})
-		// 			.catch(function (error) {
-		// 				console.log("Error code: " + error.code);
-		// 			});
-		// 	})
-		// 	.catch(function (error) {
-		// 		console.log("Error code: " + error.code);
-		// 	});
 	}).catch(function (error) {
 		// console.log("Error code: " + error.code);
 		console.log(error);
-	});
+	})
 
 
 }
